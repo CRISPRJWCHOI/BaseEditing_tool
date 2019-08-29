6d668967f5d1c91240188cd0f8f4126f20d96d1d
CRISPRJWCHOI <abmrcbioinformatics@gmail.com>
2019-08-29 10:33:45 +0900

    commit1

--- Sequence_freq.py
+++ Sequence_freq.py
@@ -1,4 +1,4 @@
-#!#!/usr/bin/env python
+#!/usr/bin/env python
 
 import os,sys
 import numpy as np
@@ -93,91 +93,102 @@ def Count_seq_freq(lPara):
     sFile_path          = lPara[1]
     sTotal_readcnt_path = lPara[2]
     dInput_fa           = lPara[3]
+    print ("Count_seq_freq: ", sProject, sFile_path, sTotal_readcnt_path)
 
-    with open('./Output/%s/Summary/Seq_freq.txt' % sProject, 'w') as Output:
+    try:
+        with open('./Output/%s/Summary/Seq_freq.txt' % sProject, 'w') as Output:
 
-        Output.write('Filename\tSeq\tMotif\tCount\tTotal_cnt\tProportion\tSubstitution\n')
+            Output.write('Filename\tSeq\tMotif\tCount\tTotal_cnt\tProportion\tSubstitution\n')
 
-        ## A project has many file. The total read count is summation in the each file.
-        for iFile_num, sFile in enumerate(os.listdir(sFile_path)):
-            #set_trace()
-            if 'aligned' in sFile:
-                # print(iFile_num)
-                sFilename = sFile.replace('_aligned_BaseEdit', '').split('.')[:-1][0] ## To search filename in other folder.
-                sTotal_readcnt_file = sFilename + '_Summary.txt'                      ## extract totral read count for the sequence frequency.
+            ## A project has many file. The total read count is summation in the each file.
+            for iFile_num, sFile in enumerate(os.listdir(sFile_path)):
+                #set_trace()
+                if 'aligned' in sFile:
+                    # print(iFile_num)
+                    sFilename = sFile.replace('_aligned_BaseEdit', '').split('.')[:-1][0] ## To search filename in other folder.
+                    sTotal_readcnt_file = sFilename + '_Summary.txt'                      ## extract totral read count for the sequence frequency.
 
-                with open(sFile_path + '/' + sFile) as aligned_BaseEdit,\
-                    open(sTotal_readcnt_path + '/' + sTotal_readcnt_file) as Total_readcnt:
-                    #print(sFile_path + '/' + sFile)
+                    with open(sFile_path + '/' + sFile) as aligned_BaseEdit,\
+                        open(sTotal_readcnt_path + '/' + sTotal_readcnt_file) as Total_readcnt:
+                        #print(sFile_path + '/' + sFile)
 
-                    iTotal_wo_indel = 0
+                        iTotal_wo_indel = 0
 
-                    for i, sRow in enumerate(Total_readcnt):
-                        if i == 0: continue
-                        lCol = sRow.replace('\n', '').split('\t')
-                        iTotal_read = int(lCol[3])                   ## This is read counts of a matched barcode.
-                        iIndel_read = int(lCol[4]) + int(lCol[5]) + int(lCol[6])
-                        iTotal_wo_indel = iTotal_read - iIndel_read  ## Total read is without indel reads
-                        break                                        ## 2 row is target, over 3 is none
+                        for i, sRow in enumerate(Total_readcnt):
+                            if i == 0: continue
+                            lCol = sRow.replace('\n', '').split('\t')
+                            iTotal_read = int(lCol[3])                   ## This is read counts of a matched barcode.
+                            iIndel_read = int(lCol[4]) + int(lCol[5]) + int(lCol[6])
+                            iTotal_wo_indel = iTotal_read - iIndel_read  ## Total read is without indel reads
+                            break                                        ## 2 row is target, over 3 is none
 
-                    lTarget_seq        = []
-                    sRef_seq           = ''
-                    dSeq_wt_extend     = {}  ## WT + motif(target sequence) + WT
+                        lTarget_seq        = []
+                        sRef_seq           = ''
+                        dSeq_wt_extend     = {}  ## WT + motif(target sequence) + WT
 
-                    for i, sRow in enumerate(aligned_BaseEdit):
+                        for i, sRow in enumerate(aligned_BaseEdit):
 
-                        lCol       = sRow.replace('\n', '').split('\t')
-                        sQuery_seq = lCol[5]
+                            lCol       = sRow.replace('\n', '').split('\t')
+                            sQuery_seq = lCol[5]
 
-                        if sRef_seq == '':   ## Reference is same in the file, so store once.
-                            sRef_seq = lCol[0]
-                            dSeq_wt_extend[sRef_seq[iWinStart - 1: iWinEnd]] = sRef_seq
+                            if sRef_seq == '':   ## Reference is same in the file, so store once.
+                                sRef_seq = lCol[0]
+                                dSeq_wt_extend[sRef_seq[iWinStart - 1: iWinEnd]] = sRef_seq
 
-                        lRef_seq_with_motif = list(sRef_seq)
-                        lRef_seq_with_motif[ iWinStart-1 : iWinEnd ] = list(sQuery_seq[ iWinStart-1 : iWinEnd ])
-                        sRef_seq_with_motif = ''.join(lRef_seq_with_motif)
+                            lRef_seq_with_motif = list(sRef_seq)
+                            lRef_seq_with_motif[ iWinStart-1 : iWinEnd ] = list(sQuery_seq[ iWinStart-1 : iWinEnd ])
+                            sRef_seq_with_motif = ''.join(lRef_seq_with_motif)
 
-                        dSeq_wt_extend[sQuery_seq[ iWinStart-1 : iWinEnd ]] = sRef_seq_with_motif
-                        lTarget_seq.append(sQuery_seq[ iWinStart-1 : iWinEnd ])
+                            dSeq_wt_extend[sQuery_seq[ iWinStart-1 : iWinEnd ]] = sRef_seq_with_motif
+                            lTarget_seq.append(sQuery_seq[ iWinStart-1 : iWinEnd ])
 
-                    iNormal  = iTotal_wo_indel - len(lTarget_seq)
-                    sRef_seq = sRef_seq[ iWinStart-1 : iWinEnd ]
+                        iNormal  = iTotal_wo_indel - len(lTarget_seq)
+                        sRef_seq = sRef_seq[ iWinStart-1 : iWinEnd ]
+                        dSeq_cnt = Counter(lTarget_seq)
 
-                    dSeq_cnt = Counter(lTarget_seq)
+                        try:
+                            iRef_cnt_in_aligned = dSeq_cnt[sRef_seq]  ## check normal sequence because substitution exists outside of window size.
+                            iNormal = iNormal + iRef_cnt_in_aligned
+                            del dSeq_cnt[sRef_seq]
+                        except KeyError:
+                            pass
 
-                    try:
-                        iRef_cnt_in_aligned = dSeq_cnt[sRef_seq]  ## check normal sequence because substitution exists outside of window size.
-                        iNormal = iNormal + iRef_cnt_in_aligned
-                        del dSeq_cnt[sRef_seq]
-                    except KeyError:
-                        pass
+                        if iNormal > 0:
+                            if sRef_seq == '':                     ## aligned result file can be none result file. So extract from input file.
+                                sRef_seq = dInput_fa[sFilename][1] ## dInput_fa[0] : full ref, dInput_fa[1] : target ref
+                                dSeq_wt_extend[sRef_seq] = dInput_fa[sFilename][0]
+                            try:
+                                Output.write('\t'.join(map(str, [sFilename, dSeq_wt_extend[sRef_seq], sRef_seq, iNormal, iTotal_wo_indel, round(iNormal/float(iTotal_wo_indel),4), 'ref_from_result']))+'\n')
+                            except Exception as e:
+                                print(e, 'line150')
+                                set_trace()
 
-                    if iNormal > 0:
-                        if sRef_seq == '':                     ## aligned result file can be none result file. So extract from input file.
-                            sRef_seq = dInput_fa[sFilename][1] ## dInput_fa[0] : full ref, dInput_fa[1] : target ref
+                        elif iNormal == 0:  ## if iNormal = 0, that means no result generation. because aligned_BaseEdit file is not contained non-read file.
+                            sRef_seq = dInput_fa[sFilename][1]
                             dSeq_wt_extend[sRef_seq] = dInput_fa[sFilename][0]
-                        try:
-                            Output.write('\t'.join(map(str, [sFilename, dSeq_wt_extend[sRef_seq], sRef_seq, iNormal, iTotal_wo_indel, round(iNormal/float(iTotal_wo_indel),4), 'ref_from_result']))+'\n')
-                        except Exception as e:
-                            print(e, 'line150')
-                            set_trace()
+                            try:
+                                Output.write('\t'.join(map(str, [sFilename, dSeq_wt_extend[sRef_seq], sRef_seq, iNormal, iTotal_wo_indel, iNormal, 'ref_from_input'])) + '\n')
+                            except Exception as e:
+                                print(e, 'line158')
+                                set_trace()
 
-                    elif iNormal == 0:  ## if iNormal = 0, that means no result generation. because aligned_BaseEdit file is not contained non-read file.
-                        sRef_seq = dInput_fa[sFilename][1]
-                        dSeq_wt_extend[sRef_seq] = dInput_fa[sFilename][0]
-                        try:
-                            Output.write('\t'.join(map(str, [sFilename, dSeq_wt_extend[sRef_seq], sRef_seq, iNormal, iTotal_wo_indel, iNormal, 'ref_from_input'])) + '\n')
-                        except Exception as e:
-                            print(e, 'line158')
-                            set_trace()
-
-                    for sSeq, iCnt in dSeq_cnt.most_common():
-                        Output.write('\t'.join(map(str, [sFilename, dSeq_wt_extend[sSeq], sSeq, iCnt, iTotal_wo_indel, round(iCnt/float(iTotal_wo_indel),4), 'alt']))+'\n')
-
-                #END: for
-            #END: with
-        #END: for
-    #END: with
+                        for sSeq, iCnt in dSeq_cnt.most_common():
+                            try:
+                                Output.write('\t'.join(map(str, [sFilename, dSeq_wt_extend[sSeq], sSeq, iCnt, iTotal_wo_indel, round(iCnt/float(iTotal_wo_indel),4), 'alt']))+'\n')
+                            except Exception as e:
+                                print(lPara[0], sFilename)
+                                print(iCnt, iTotal_wo_indel)
+                                print(e, 'line175')
+                                #pass
+                                set_trace()
+                    #END: for
+                #END: with
+            #END: for
+        #END: with
+    except Exception as e:
+        print(e)
+        print("Error in the input: ", sProject, sFilename, sTotal_readcnt_file)
+        pass
 #END: def
 
 
@@ -224,6 +235,7 @@ def Count_group():
     ## Make dictionary to sum the total reads count of the group. The total reads count is always same in their group.
     with open('Group_list.txt') as Group_list:
         for sGroupname in Group_list:
+            if sGroupname[0] == "#": continue
             sGroupname = sGroupname.replace('\n', '').strip()
             if not os.path.isdir('./Output/Group_result'): os.mkdir('./Output/Group_result')
 
@@ -236,7 +248,10 @@ def Count_group():
                         for sRow in SeqFreq:
                             lCol            = sRow.replace('\n', '').split('\t')
                             sFilename       = lCol[0]
-                            iTotal_read_cnt = int(lCol[4])
+                            try:
+                                iTotal_read_cnt = int(lCol[4])
+                            except IndexError:
+                                set_trace()
                             dSelect_one_total_cnt[sFilename] = iTotal_read_cnt
 
                         for sFilename, iTotal_read_cnt in dSelect_one_total_cnt.items():
@@ -247,7 +262,7 @@ def Count_group():
 
     with open('Group_list.txt') as Group_list:
         for sGroupname in Group_list:
-
+            if sGroupname[0] == "#": continue
             sGroupname = sGroupname.replace('\n', '').strip()
             dSeq_freq  = OrderedDict()   ## ('GECKO_6367_GATCTGCTC', ['GECKO_6367', 'GATCTGCTC', 2, 156, '0.0128']),
                                          ## Unique key, only one list.
@@ -306,6 +321,7 @@ def Trim_data():
     """
     with open('Group_list.txt') as Group_list:
         for sGroupname in Group_list:
+            if sGroupname[0] == "#": continue
             sGroupname = sGroupname.replace('\n', '').strip()
             dSeq_freq  = OrderedDict()
 
@@ -390,7 +406,7 @@ def Main():
 
     logging.info('Program Start')
     #Sum_all_freq() ## deprecated
-
+    #"""
     logging.info('Make commands for a multiple processing')
     lPara = []
     with open('Project_list.txt') as Project_list:
@@ -410,7 +426,7 @@ def Main():
     p = mp.Pool(iCore)
     p.map_async(Count_seq_freq, lPara).get()
     logging.info('Multiple processing End')
-
+    #"""
     logging.info('Count group Start')
     Count_group()
     logging.info('Count group End')
